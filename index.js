92ea7007d6105da3ea00552d6a33bd105ab90553
Aakil Fernandes <aakilfernandes@gmail.com>
2017-04-29 15:21:37 -0400

    upgrade arguguard

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ Amorph.loadPlugin(amorphBufferPlugin)
 Amorph.ready()
 
 exports.encrypt = function encrypt(plaintext, key, iv) {
-  arguguard('encrypt', [Amorph, Amorph, Amorph], arguments)
+  arguguard('encrypt', ['Amorph', 'Amorph', 'Amorph'], arguments)
   const aesCbc = new aes.ModeOfOperation.cbc(key.to('uint8Array'), iv.to('uint8Array'))
   return new Amorph(aesCbc.encrypt(plaintext.to('uint8Array')), 'uint8Array')
 }
 
 exports.decrypt = function decrypt(ciphertext, key, iv) {
-  arguguard('decrypt', [Amorph, Amorph, Amorph], arguments)
+  arguguard('decrypt', ['Amorph', 'Amorph', 'Amorph'], arguments)
   const aesCbc = new aes.ModeOfOperation.cbc(key.to('uint8Array'), iv.to('uint8Array'))
   return new Amorph(aesCbc.decrypt(ciphertext.to('uint8Array')), 'uint8Array')
 }
